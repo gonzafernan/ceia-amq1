75dea2cb0262b75de968f3473a237d65c19ec758
Gonzalo G. Fernandez <fernandez.gfg@gmail.com>
2024-12-10 23:30:05 +0100

    Integrate MLflow in normalization airflow job

--- airflow/dags/etl_process.py
+++ airflow/dags/etl_process.py
@@ -1,3 +1,5 @@
+"""ETL process for Stroke Detection with Apache Airflow DAG"""
+
 import datetime
 
 from airflow.decorators import task
@@ -6,11 +8,11 @@ from airflow import DAG
 MARKDOWN_TEXT = """
 ### ETL Process for Stroke Prediction Dataset
 
-This DAG extracts information from Kaggle's  
-[Stroke Prediction Dataset](https://www.kaggle.com/datasets/fedesoriano/stroke-prediction-dataset). 
+Extract, Transform, and Load (ETL). This DAG extracts information from Kaggle's
+[Stroke Prediction Dataset](https://www.kaggle.com/datasets/fedesoriano/stroke-prediction-dataset).
 It preprocesses the data by creating dummy variables and scaling numerical features.
-    
-After preprocessing, the data is saved back into a S3 bucket as two separate CSV files: one for training and one for 
+
+After preprocessing, the data is saved back into a S3 bucket as two separate CSV files: one for training and one for
 testing. The split between the training and testing datasets is 70/30 and they are stratified.
 """
 
@@ -23,7 +25,6 @@ default_args = {
     "retry_delay": datetime.timedelta(minutes=5),
     "dagrun_timeout": datetime.timedelta(minutes=15),
 }
-
 
 
 # Initialize the DAG
@@ -50,7 +51,7 @@ with DAG(
         import os
 
         dataset_zip_name = "stroke-prediction-dataset"
-        dataset_url = f"http://192.168.1.185:12000/{dataset_zip_name}.zip" 
+        dataset_url = f"http://192.168.1.185:12000/{dataset_zip_name}.zip"
         dataset_csv_name = "healthcare-dataset-stroke-data"
         dataset_s3_path = f"s3://data/raw/{dataset_csv_name}.csv"
 
@@ -61,22 +62,21 @@ with DAG(
 
         return dataset_s3_path
 
-
     @task.virtualenv(
-        task_id="make_dummies_variables",
-        requirements=["awswrangler", "pandas", "mlflow"],
-        system_site_packages=True
+        task_id="make_dummies_variables", requirements=["awswrangler", "pandas", "mlflow"], system_site_packages=True
     )
     def make_dummies_variables():
         """Convert categorical variables into one-hot encoding."""
         # import json
         import datetime
+
         # import boto3
         # import botocore.exceptions
         import mlflow
 
         import awswrangler as wr
         import pandas as pd
+
         # import numpy as np
         # from airflow.models import Variable
 
@@ -92,7 +92,7 @@ with DAG(
 
         # Fill NaN
         df.dropna(inplace=True, ignore_index=True)
-        df['bmi'].fillna(df['bmi'].median(), inplace=True)
+        df["bmi"].fillna(df["bmi"].median(), inplace=True)
 
         # Cathegoric variables transformation
         print("Applying One-Hot encoding to:")
@@ -140,43 +140,48 @@ with DAG(
         # data_dict['categorical_columns'] = categories_list
         # data_dict['columns_dtypes'] = {k: str(v) for k, v in dataset_log.dtypes.to_dict().items()}
         # data_dict['columns_dtypes_after_dummy'] = {k: str(v) for k, v in dataset_with_dummies_log.dtypes
-                                                                                                #  .to_dict()
-                                                                                                #  .items()}
+        #  .to_dict()
+        #  .items()}
 
-    #     category_dummies_dict = {}
-    #     for category in categories_list:
-    #         category_dummies_dict[category] = np.sort(dataset_log[category].unique()).tolist()
+        #     category_dummies_dict = {}
+        #     for category in categories_list:
+        #         category_dummies_dict[category] = np.sort(dataset_log[category].unique()).tolist()
 
-    #     data_dict['categories_values_per_categorical'] = category_dummies_dict
+        #     data_dict['categories_values_per_categorical'] = category_dummies_dict
 
-    #     data_dict['date'] = datetime.datetime.today().strftime('%Y/%m/%d-%H:%M:%S"')
-    #     data_string = json.dumps(data_dict, indent=2)
+        #     data_dict['date'] = datetime.datetime.today().strftime('%Y/%m/%d-%H:%M:%S"')
+        #     data_string = json.dumps(data_dict, indent=2)
 
-    #     client.put_object(
-    #         Bucket='data',
-    #         Key='data_info/data.json',
-    #         Body=data_string
-    #     )
+        #     client.put_object(
+        #         Bucket='data',
+        #         Key='data_info/data.json',
+        #         Body=data_string
+        #     )
 
-        mlflow.set_tracking_uri('http://mlflow:5000')
+        mlflow.set_tracking_uri("http://mlflow:5000")
         experiment = mlflow.set_experiment("ETL Stroke Prediction")
 
-        mlflow.start_run(run_name='ETL_run_' + datetime.datetime.today().strftime('%Y/%m/%d-%H:%M:%S"'),
-                         experiment_id=experiment.experiment_id,
-                         tags={"experiment": "etl", "dataset": "Stroke Prediction"},
-                         log_system_metrics=True)
+        mlflow.start_run(
+            run_name="ETL_run_" + datetime.datetime.today().strftime('%Y/%m/%d-%H:%M:%S"'),
+            experiment_id=experiment.experiment_id,
+            tags={"experiment": "etl", "dataset": "Stroke Prediction"},
+            log_system_metrics=True,
+        )
 
-        mlflow_dataset = mlflow.data.from_pandas(df,
-                                                 source="https://www.kaggle.com/datasets/fedesoriano/stroke-prediction-dataset",
-                                                 targets="stroke",
-                                                 name="stroke_data_complete")
-        mlflow_dataset_dummies = mlflow.data.from_pandas(df_dummies,
-                                                         source="https://www.kaggle.com/datasets/fedesoriano/stroke-prediction-dataset",
-                                                         targets="stroke",
-                                                         name="stroke_data_complete_with_dummies")
+        mlflow_dataset = mlflow.data.from_pandas(
+            df,
+            source="https://www.kaggle.com/datasets/fedesoriano/stroke-prediction-dataset",
+            targets="stroke",
+            name="stroke_data_complete",
+        )
+        mlflow_dataset_dummies = mlflow.data.from_pandas(
+            df_dummies,
+            source="https://www.kaggle.com/datasets/fedesoriano/stroke-prediction-dataset",
+            targets="stroke",
+            name="stroke_data_complete_with_dummies",
+        )
         mlflow.log_input(mlflow_dataset, context="Dataset")
         mlflow.log_input(mlflow_dataset_dummies, context="Dataset")
-
 
     @task.virtualenv(
         task_id="split_dataset",
@@ -197,7 +202,6 @@ with DAG(
 
         dataset = wr.s3.read_csv(dataset_dummies_path)
 
-
         # test_size = Variable.get("test_size_heart")
         test_size = 0.2
         # target_col = Variable.get("stroke")
@@ -208,7 +212,7 @@ with DAG(
 
         # fix classes imbalance
         smote = SMOTE(sampling_strategy="minority")
-        X_smote , y_smote = smote.fit_resample(X, y)
+        X_smote, y_smote = smote.fit_resample(X, y)
 
         X_train, X_test, y_train, y_test = train_test_split(
             X_smote, y_smote, test_size=test_size, stratify=y_smote, random_state=42
@@ -222,7 +226,6 @@ with DAG(
         save_to_csv(y_train, f"s3://data/final/train/{dataset_csv_name}_y_train.csv")
         save_to_csv(y_test, f"s3://data/final/test/{dataset_csv_name}_y_test.csv")
 
-
     @task.virtualenv(
         task_id="normalize_numerical_features",
         requirements=["awswrangler", "scikit-learn", "mlflow"],
@@ -233,7 +236,7 @@ with DAG(
         Standardization of numerical columns
         """
         # import json
-        # import mlflow
+        import mlflow
         # import boto3
         # import botocore.exceptions
 
@@ -250,7 +253,7 @@ with DAG(
         X_train = wr.s3.read_csv(f"s3://data/final/train/{dataset_csv_name}_X_train.csv")
         X_test = wr.s3.read_csv(f"s3://data/final/test/{dataset_csv_name}_X_test.csv")
 
-        scaler=StandardScaler()
+        scaler = StandardScaler()
         X_train_arr = scaler.fit_transform(X_train)
         X_test_arr = scaler.fit_transform(X_test)
 
@@ -279,18 +282,17 @@ with DAG(
 
     #     client.put_object(Bucket="data", Key="data_info/data.json", Body=data_string)
 
-    #     mlflow.set_tracking_uri("http://mlflow:5000")
-    #     experiment = mlflow.set_experiment("Heart Disease")
+        mlflow.set_tracking_uri("http://mlflow:5000")
+        experiment = mlflow.set_experiment("ETL Stroke Prediction")
 
-    #     # Obtain the last experiment run_id to log the new information
-    #     list_run = mlflow.search_runs([experiment.experiment_id], output_format="list")
+        # Obtain the last experiment run_id to log the new information
+        list_run = mlflow.search_runs([experiment.experiment_id], output_format="list")
 
-    #     with mlflow.start_run(run_id=list_run[0].info.run_id):
-
-    #         mlflow.log_param("Train observations", X_train.shape[0])
-    #         mlflow.log_param("Test observations", X_test.shape[0])
-    #         mlflow.log_param("Standard Scaler feature names", sc_X.feature_names_in_)
-    #         mlflow.log_param("Standard Scaler mean values", sc_X.mean_)
-    #         mlflow.log_param("Standard Scaler scale values", sc_X.scale_)
+        with mlflow.start_run(run_id=list_run[0].info.run_id):
+            mlflow.log_param("Train observations", X_train.shape[0])
+            mlflow.log_param("Test observations", X_test.shape[0])
+            mlflow.log_param("Standard Scaler feature names", scaler.feature_names_in_)
+            mlflow.log_param("Standard Scaler mean values", scaler.mean_)
+            mlflow.log_param("Standard Scaler scale values", scaler.scale_)
 
     fetch_save_dataset() >> make_dummies_variables() >> split_dataset() >> normalize_data()
